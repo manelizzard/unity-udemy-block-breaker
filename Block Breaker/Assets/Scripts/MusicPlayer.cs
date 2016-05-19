89f50d137700721e3e407e05115ad02545b805ae
Manel Simon <manelizzard@gmail.com>
2016-05-19 23:39:55 +0200

    * Music player persisting over scenes

---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Start () {
+
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = null;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
